3617dd8733729a0575372570dc55881eca9248bb
NeomiA1 <neomianikin123@gmail.com>
2025-11-27 17:47:45 +0200

    chanse static list

--- BL/Movie.cs
+++ BL/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Movies.DAL; 
 
 namespace Movies.BL
 {
@@ -17,76 +18,25 @@ namespace Movies.BL
         public string Genre { get; set; }
         public string PhotoUrl { get; set; }
 
-        public static List<Movie> moviesList = new List<Movie>();
-
-        // Seed movies for testing (לא חובה במטלה, אבל מעולה לבדיקה)
-        static Movie()
-        {
-            moviesList.Add(new Movie
-            {
-                Id = 1,
-                Title = "Inception",
-                Rating = 8.8,
-                Income = 830,
-                ReleaseYear = 2010,
-                Duration = 148,
-                Language = "English",
-                Description = "Dreams inside dreams",
-                Genre = "Sci-Fi",
-                PhotoUrl = ""
-            });
-
-            moviesList.Add(new Movie
-            {
-                Id = 2,
-                Title = "Toy Story",
-                Rating = 8.3,
-                Income = 400,
-                ReleaseYear = 1995,
-                Duration = 81,
-                Language = "English",
-                Description = "Animated toys",
-                Genre = "Animation",
-                PhotoUrl = ""
-            });
-
-            moviesList.Add(new Movie
-            {
-                Id = 3,
-                Title = "Test Movie",
-                Rating = 5.2,
-                Income = 50,
-                ReleaseYear = 2005,
-                Duration = 90,
-                Language = "English",
-                Description = "Test",
-                Genre = "Drama",
-                PhotoUrl = ""
-            });
-        }
+                private static readonly MovieDAL dal = new MovieDAL();
 
         public bool Insert()
         {
-            foreach (Movie movie in moviesList)
-            {
-                if (movie.Id == this.Id)
-                {
-                    throw new Exception("Movie is already exists");
-                }
-            }
 
-            moviesList.Add(this);
-            return true;
+            bool success = dal.InsertMovie(this);
+            return success;
         }
 
         public static List<Movie> Read()
         {
-            return moviesList;
+            return dal.GetAllMovies();
         }
 
         public static List<Movie> ReadByRating(double minRating)
         {
-            return moviesList
+            List<Movie> all = dal.GetAllMovies();
+
+            return all
                 .Where(m => m.Rating >= minRating)
                 .ToList();
         }
